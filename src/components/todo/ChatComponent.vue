c79d7f391b5c74a1808c0f8dd6337bd1fb7da7a9
Nguyễn Văn Việt <viet.nguyen_van@napaglobal.com>
2022-09-29 08:32:07 +0700

    create message socket

--- src/components/todo/ChatComponent.vue
+++ src/components/todo/ChatComponent.vue
@@ -12,10 +12,11 @@
         :time="item.time"
       ></message-chat>
     </div>
+    <div class="typing">Typing</div>
     <div class="input__message">
       <v-text-field
         v-model="inputMessage"
-        @keydown.enter="sendMessage"
+        @keydown="sendMessage"
       ></v-text-field>
       <v-icon class="icon__send" @click="sendMessage">mdi-send</v-icon>
     </div>
@@ -29,7 +30,9 @@ export default {
   name: "chat-component",
   data() {
     return {
+      room: "0001",
       inputMessage: "",
+      typing: false,
       messageData: [
         {
           title: "John Doe",
@@ -50,34 +53,119 @@ export default {
     };
   },
   methods: {
-    sendMessage() {
-      if (this.inputMessage !== "") {
-        this.messageData.push({
-          title: "John Cena",
-          message: this.inputMessage,
-          isUser: true,
-          image: "https://technext.github.io/elaadmin/images/avatar/64-2.jpg",
-          time: "11.12 am",
-        });
-        this.inputMessage = "";
-        let a = this.$refs.chat;
-        setTimeout(() => {
-          a.scrollTop = a.scrollHeight;
-        }, 0);
+    sendMessage(e) {
+      if (e.keyCode == 13) {
+        if (this.inputMessage !== "") {
+          this.$socket.emit("sendMessage", {
+            room: this.room,
+            message: this.inputMessage,
+          });
+          this.messageData.push({
+            title: "Nguoi gui",
+            message: this.inputMessage,
+            isUser: true,
+            image: "https://technext.github.io/elaadmin/images/avatar/64-2.jpg",
+            time: "11.12 am",
+          });
+          this.inputMessage = "";
+          let a = this.$refs.chat;
+          setTimeout(() => {
+            a.scrollTop = a.scrollHeight;
+          }, 0);
+        }
+      } else {
+        this.onKeyDownNotEnter();
+      }
+    },
+    timeoutFunction() {
+      this.$socket.emit("typing", { room: this.room, typing: this.typing });
+    },
+    onKeyDownNotEnter() {
+      let timeout;
+      if (this.typing == false) {
+        this.typing = true;
+        this.$socket.emit("typing", { room: this.room, typing: this.typing });
+        timeout = setTimeout(this.timeoutFunction(), 5000);
+      } else {
+        clearTimeout(timeout);
+        timeout = setTimeout(this.timeoutFunction(), 5000);
       }
     },
   },
   mounted() {
-    let a = this.$refs.chat;
     setTimeout(() => {
-      a.scrollTop = a.scrollHeight;
+      this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
     }, 0);
+  },
+  created() {
+    this.$socket.emit("room", this.room);
+  },
+  sockets: {
+    connect() {
+      // Fired when the socket connects.
+      this.isConnected = true;
+      console.log("server connected");
+    },
+    disconnect() {
+      this.isConnected = false;
+      console.log("server disconnected");
+    },
+    // initRoom: function (message) {
+    //   // Đây là nơi nhận cái even initRoom với param là message mà server emit về
+    //   this.messageData.push({
+    //     title: "John Doe",
+    //     message: message,
+    //     isUser: false,
+    //     image: "https://technext.github.io/elaadmin/images/avatar/64-1.jpg",
+    //     time: "11.11 am",
+    //   });
+    // },
+    // statusRoom: function (message) {
+    //   this.messageData.push({
+    //     title: "John Doe",
+    //     message: message,
+    //     isUser: false,
+    //     image: "https://technext.github.io/elaadmin/images/avatar/64-1.jpg",
+    //     time: "11.11 am",
+    //   });
+    // },
+    receiveMessage: function (data) {
+      console.log("message nhan", data);
+      //nhận tín nhắn từ ng khác trong phòng, push tin nhắn vào mảng ban đầu
+      this.messageData.push({
+        title: "Server",
+        message: data,
+        isUser: false,
+        image: "https://technext.github.io/elaadmin/images/avatar/64-1.jpg",
+        time: "11.11 am",
+      });
+      setTimeout(() => {
+        this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+      }, 0);
+    },
+    receiveTyping: function (data) {
+      console.log("typing nhan", data);
+      //nhận tín nhắn từ ng khác trong phòng, push tin nhắn vào mảng ban đầu
+      // this.messageData.push({
+      //   title: "Server",
+      //   message: data,
+      //   isUser: false,
+      //   image: "https://technext.github.io/elaadmin/images/avatar/64-1.jpg",
+      //   time: "11.11 am",
+      // });
+      // setTimeout(() => {
+      //   this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+      // }, 0);
+    },
   },
 };
 </script>
 <style lang="scss">
 .chat-home {
   gap: 20px;
+  .typing {
+    text-align: center;
+  }
 }
 .chat__content {
   display: flex;
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,16 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueAxios from "vue-axios";
 import axios from "axios";
+import VueSocketIO from "vue-socket.io";
 
 Vue.config.productionTip = false;
+
+// console.log(VueSocketIO);
+Vue.use(
+  new VueSocketIO({
+    connection: "http://localhost:3000",
+  })
+);
 
 Vue.use(VueAxios, axios);
 new Vue({
